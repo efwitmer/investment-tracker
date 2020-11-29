1c41921709204b0b34d6c5c51f0d305da8a587fa
Emma Witmer <emmswitmer@gmail.com>
2020-11-28 21:25:34 -0600

    console logging the data and dev.sh

--- dev.sh
+++ dev.sh
@@ -4,6 +4,10 @@
 
 #!/usr/bin/env bash
 
-export environment="DEV";
+export ENVIRONMENT="DEV"
+export DB_HOST="127.0.0.1"
+export DB_PW=""
+export DB_USER="emma"
+export DB_NAME="practiceDB"
 
 node .
