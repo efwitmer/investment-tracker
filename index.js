743437868939a19382ddfe52188bee6195832c8c
Nathaniel Witmer <beckeswins@gmail.com>
2020-11-28 22:29:41 -0600

    add a route to add an order

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const init = async () => {
 									price_per_unit,
 									quantity,
 									buy_sell,
-									field
+									industry
 								FROM investments
 								ORDER BY datetime DESC;`;
 				db.query(query, [], (err, results) => {
@@ -69,6 +69,76 @@ const init = async () => {
 						},
 				        statusCode: 200
 				    };
+					if (err) {
+						response.statusCode = 503;
+					} else {
+						response.body = JSON.stringify(results);
+					}
+					return resolve(response);
+				});
+			})
+		}
+	});
+
+	// Add order
+	server.route({
+		method: 'POST',
+		path: '/add_order',
+		handler:  (request, h) => {
+			return new Promise((resolve, reject) => {
+				dev && console.log(`POST /add_order`);
+				const response = {
+					headers: {
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Credentials': true,
+						'Cache-Control': 'no-cache'
+					},
+			        statusCode: 200
+			    };
+				const payload 		 = request.payload;
+				if (!payload) {
+					response.statusCode = 400;
+					response.message = "Missing request payload";
+					return resolve(response);
+				}
+				const asset_type 	 = payload.asset_type;
+				const ticker 		 = payload.ticker;
+				const asset_name 	 = payload.asset_name;
+				const datetime 		 = payload.datetime;
+				const fee 			 = payload.fee || 0;
+				const price_per_unit = payload.price_per_unit;
+				const quantity 		 = payload.quantity;
+				const buy_sell 		 = payload.buy_sell;
+				const industry 		 = payload.industry;
+
+				if (!asset_type || !ticker || !asset_name || !datetime|| !price_per_unit || !quantity || !buy_sell || !industry) {
+					response.statusCode = 400;
+					response.message = `Missing one or more of:
+										asset_type: ${asset_type}
+										ticker: ${ticker}
+										asset_name: ${asset_name}
+										datetime: ${datetime}
+										price_per_unit: ${price_per_unit}
+										quantity: ${quantity}
+										buy_sell: ${buy_sell}
+										industry: ${industry}`;
+					return resolve(response);
+				}
+
+				const query = `INSERT INTO investments (
+									asset_type,
+									ticker,
+									asset_name,
+									datetime,
+									fee,
+									price_per_unit,
+									quantity,
+									buy_sell,
+									industry
+								)
+								VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+				const params = [asset_type, ticker, asset_name, datetime, fee, price_per_unit, quantity, buy_sell, industry]
+				db.query(query, params, (err, results) => {
 					if (err) {
 						response.statusCode = 503;
 					} else {
